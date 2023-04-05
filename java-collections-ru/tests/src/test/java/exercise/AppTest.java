9aaca112a153b4fcb5ada6344499b720ee05ee38
dariakoval <dasharibkavmore666@mail.ru>
2023-04-05 17:24:34 +0400

    update file

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -4,22 +4,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import java.util.Arrays;
 import java.util.ArrayList;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class AppTest {
-    List<Integer> numbers;
-    @BeforeEach
-    void initIntegerList() {
-        this.numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-    }
-
     @Test
     void testTake() {
         // BEGIN
-        List<Integer> result = App.take(numbers, 2);
-        List<Integer> rightResult = Implementations.right(numbers, 2);
-        assertThat(result).isEqualTo(rightResult);
+        List<Integer> numbers1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        List<Integer> expected1 = new ArrayList<>(Arrays.asList(1, 2));
+        List<Integer> result1 = App.take(numbers1, 2);
+        assertThat(result1).isEqualTo(expected1);
+
+        List<Integer> numbers2 = new ArrayList<>(Arrays.asList(1, 2, 3));
+        List<Integer> expected2 = new ArrayList<>(Arrays.asList(1, 2, 3));
+        List<Integer> result2 = App.take(numbers2, 7);
+        assertThat(result2).isEqualTo(expected2);
+
+        List<Integer> numbers3 = new ArrayList<>();
+        List<Integer> expected3 = new ArrayList<>();
+        List<Integer> result3 = App.take(numbers3, 4);
+        assertThat(result3).isEqualTo(expected3);
         // END
     }
 }
