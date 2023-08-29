1f43893b4e164865ac5cd1104ea77ae4c888f225
@hexlet/cli <support@hexlet.io>
2023-08-30 00:06:47 +0400

    submit java-web-ru/session

--- java-web-ru/session/src/main/java/exercise/controller/SessionsController.java
+++ java-web-ru/session/src/main/java/exercise/controller/SessionsController.java
@@ -1,54 +1,46 @@
 package exercise.controller;
 
 import java.util.Collections;
-import java.util.Objects;
-
 import exercise.dto.MainPage;
 import exercise.dto.LoginPage;
 import exercise.repository.UsersRepository;
 import static exercise.util.Security.encrypt;
 
-import exercise.util.NamedRoutes;
-import exercise.util.Security;
 import io.javalin.http.Context;
 
 public class SessionsController {
 
     // BEGIN
     public static void index(Context ctx) {
-        var cookie = ctx.cookie("currentUser") == null ? "" : ctx.cookie("currentUser");
-        var page = new MainPage(cookie);
-        ctx.cookie("currentUser", cookie);
+        var user = ctx.sessionAttribute("user");
+        var page = new MainPage(user);
         ctx.render("index.jte", Collections.singletonMap("page", page));
     }
 
     public static void build(Context ctx) {
-        var cookie = ctx.cookie("currentUser") == null ? "" : ctx.cookie("currentUser");
-        var page = new LoginPage(cookie, "");
+        var page = new LoginPage("", "");
         ctx.render("build.jte", Collections.singletonMap("page", page));
     }
 
     public static void create(Context ctx) {
         var name = ctx.formParam("name");
-        var password = Security.encrypt(Objects.requireNonNull(ctx.formParam("password")));
+        var password = ctx.formParam("password");
 
         var user = UsersRepository.findByName(name);
 
-        if (user == null || !user.getPassword().equals(password)) {
-            var error = "Wrong username or password";
-            var page = new LoginPage(name, error);
+        if (user != null && user.getPassword().equals(encrypt(password))) {
+            ctx.sessionAttribute("user", user.getName());
+            ctx.redirect("/");
+        } else {
+            var errorMessage = "Wrong username or password";
+            var page = new LoginPage(name, errorMessage);
             ctx.render("build.jte", Collections.singletonMap("page", page));
-            return;
         }
-
-        ctx.cookie("currentUser", name);
-        ctx.redirect(NamedRoutes.rootPath());
-
     }
 
     public static void destroy(Context ctx) {
-        ctx.cookie("currentUser", "");
-        ctx.redirect(NamedRoutes.rootPath());
+        ctx.sessionAttribute("user", null);
+        ctx.redirect("/");
     }
     // END
 }
