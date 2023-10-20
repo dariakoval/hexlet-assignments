18acadaa3f923ec3ae6eab7d254b779ee40249bd
@hexlet/cli <support@hexlet.io>
2023-10-20 12:26:00 +0400

    submit java-spring-ru/crud

--- java-spring-ru/crud/src/main/java/exercise/handler/GlobalExceptionHandler.java
+++ java-spring-ru/crud/src/main/java/exercise/handler/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package exercise.handler;
 
 import exercise.exception.BadRequestException;
-import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
