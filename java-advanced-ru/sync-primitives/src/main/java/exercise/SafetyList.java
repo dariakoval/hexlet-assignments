a4b2898c31b79dd7d590739f45c83d69a00d82b1
@hexlet/cli <support@hexlet.io>
2023-10-22 17:05:15 +0400

    submit java-advanced-ru/sync-primitives

---
+++ java-advanced-ru/sync-primitives/src/main/java/exercise/SafetyList.java
@@ -0,0 +1,24 @@
+package exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class SafetyList {
+    // BEGIN
+    private List<Integer> array = new ArrayList<>();
+
+    public SafetyList() {
+    }
+
+    public synchronized void add(Integer number) {
+        array.add(number);
+    }
+
+    public Integer get(int index) {
+        return array.get(index);
+    }
+    public int getSize() {
+        return array.size();
+    }
+    // END
+}
